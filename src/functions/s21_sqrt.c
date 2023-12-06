d53a8adc61583527ca8b2594aefaf20185d09e94
Adeline Hortensia <adelineh@ox-d5.msk.21-school.ru>
2023-12-06 18:45:50 +0300

    sqrt fix

--- src/functions/s21_sqrt.c
+++ src/functions/s21_sqrt.c
@@ -7,7 +7,7 @@ long double s21_sqrt(double x) {
   else if (x < 0)
     return S21_NAN;
   long double z = 1.0;
-  for (int i = 1; i <= 10; i++) {
+  for (int i = 1; i <= 15; i++) {
     z -= (z * z - x) / (2 * z);
   }
   return z;
