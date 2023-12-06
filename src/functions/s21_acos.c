a514158793fd90fb6aee1e30feace014ed68c27c
Adeline Hortensia <adelineh@ox-d5.msk.21-school.ru>
2023-12-06 18:35:29 +0300

    cppcheck

--- src/functions/s21_acos.c
+++ src/functions/s21_acos.c
@@ -2,7 +2,7 @@
 
 long double s21_acos(double x) {
   long double sinX = s21_sqrt(1 - x * x);
-  long double result = S21_PI - s21_asin(sinX);
+  long double result;
   if (x < -1 || x > 1) {
     result = S21_NAN;
   } else if (x == 0) {
--- src/functions/s21_floor.c
+++ src/functions/s21_floor.c
@@ -2,10 +2,10 @@
 
 long double s21_floor(double x) {
   long double result;
-  int status = 0;
   if (S21_NAN == s21_fabs(x) || S21_INF == s21_fabs(x)) {
     result = x;
   } else {
+    int status = 0;
     result = (long long int)x;
     if (result == x) status = 1;
     if (status == 0) {
